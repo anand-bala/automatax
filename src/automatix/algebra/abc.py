8afc37895a55519968145d0bad0b55699c29dee9
Anand Balakrishnan <anandbala1597@gmail.com>
2024-10-25 18:53:23 -0700

    feat: implement boolean polynomials as BDDs
    - Use the `dd` module to build BDDs for Boolean polynomials
      - Minimize the interface, keeping the things like `declare` and `let`.
      - Implement dunder methods for the `__add__`, and `__mul__`.

--- src/automatix/algebra/abc.py
+++ src/automatix/algebra/abc.py
@@ -1,32 +1,32 @@
-from abc import ABC, abstractmethod
-from typing import Generic, TypeVar
+from abc import ABC, abstractclassmethod, abstractmethod
+from typing import Generic, Mapping, Optional, Self, TypeVar
 
-from typing_extensions import ClassVar
+from typing_extensions import ClassVar, override
 
-_S = TypeVar("_S")
+S = TypeVar("S")
 
 
-class AbstractSemiring(ABC, Generic[_S]):
+class AbstractSemiring(ABC, Generic[S]):
     """Base semiring class"""
 
     @staticmethod
     @abstractmethod
-    def zero() -> _S:
+    def zero() -> S:
         """Return the additive identity"""
 
     @staticmethod
     @abstractmethod
-    def one() -> _S:
+    def one() -> S:
         """Return the multiplicative identity"""
 
     @staticmethod
     @abstractmethod
-    def add(x: _S, y: _S) -> _S:
+    def add(x: S, y: S) -> S:
         """Return the addition of two elements in the semiring"""
 
     @staticmethod
     @abstractmethod
-    def multiply(x: _S, y: _S) -> _S:
+    def multiply(x: S, y: S) -> S:
         """Return the multiplication of two elements in the semiring"""
 
     is_additively_idempotent: ClassVar[bool] = False
@@ -35,10 +35,86 @@ class AbstractSemiring(ABC, Generic[_S]):
     is_simple: ClassVar[bool] = False
 
 
-class AbstractNegation(ABC, Generic[_S]):
-    """A negation function on `_S` is an involution on `_S`"""
+class AbstractNegation(ABC, Generic[S]):
+    """A negation function on `S` is an involution on `S`"""
 
     @staticmethod
     @abstractmethod
-    def negate(x: _S) -> _S:
+    def negate(x: S) -> S:
         """An involution in the algebra"""
+
+
+Ctx = TypeVar("Ctx")
+
+
+class AbstractPolynomial(ABC, Generic[S, Ctx]):
+    """A polynomial with coefficients and the value of variables in `S`, where `S` is a semiring."""
+
+    @property
+    @abstractmethod
+    def context(self) -> Ctx:
+        """Give the polynomial's context manager"""
+
+    @property
+    @abstractmethod
+    def support(self) -> set[str]:
+        """Return the list of variables with non-zero coefficients in the polynomial"""
+        ...
+
+    @abstractmethod
+    def declare(self, vars: set[str]) -> None:
+        """Declare a list of variables that define the support of the polynomial."""
+
+    @abstractmethod
+    @classmethod
+    def zero(cls, manager: Optional[Ctx] = None) -> Self:
+        """Return a constant `0` polynomial"""
+
+    @abstractmethod
+    @classmethod
+    def const(cls, value: bool, manager: Optional[Ctx] = None) -> Self:
+        """Return a constant"""
+
+    @abstractmethod
+    def let(self, mapping: Mapping[str, S | Self]) -> Self:
+        """Substitute variables with constants or other polynomials."""
+
+    @abstractmethod
+    def eval(self, mapping: Mapping[str, S]) -> S:
+        """Evaluate the polynomial with the given variable values.
+
+        !!! note
+
+            Asserts that all variables that form the support of the polynomial are used.
+        """
+
+    @abstractmethod
+    def add(self, other: S | Self) -> Self:
+        """Return the addition (with appropriate semiring) of two polynomials."""
+
+    @abstractmethod
+    def multiply(self, other: S | Self) -> Self:
+        """Return the multiplication (with appropriate semiring) of two polynomials."""
+
+    @abstractmethod
+    def is_top(self) -> bool:
+        """Returns `True` if the Polynomial is just the additive identity in the semiring."""
+
+    @abstractmethod
+    def is_bottom(self) -> bool:
+        """Returns `True` if the Polynomial is just the multiplicative identity in the semiring."""
+
+    def __add__(self, other: S | Self) -> Self:
+        return self.add(other)
+
+    def __radd__(self, other: S | Self) -> Self:
+        return self.add(other)
+
+    def __mul__(self, other: S | Self) -> Self:
+        return self.multiply(other)
+
+    def __rmul__(self, other: S | Self) -> Self:
+        return self.multiply(other)
+
+    def __call__(self, mapping: Mapping[str, S | Self]) -> S | Self:
+        return self.let(mapping)
---
+++ src/automatix/algebra/polynomials/boolean.py
@@ -0,0 +1,141 @@
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Iterable, Mapping, Optional, Self, final
+
+from typing_extensions import override
+
+from automatix.algebra.abc import AbstractPolynomial
+from automatix.algebra.semiring import BooleanAlgebra
+
+if TYPE_CHECKING:
+    import dd.autoref as bddlib
+else:
+    try:
+        import dd.cudd as bddlib  # pyright: ignore[reportMissingImports]
+    except ImportError:
+        import dd.autoref as bddlib
+
+
+@dataclass
+class Context:
+    variables: set[str] = field(default_factory=set)
+    bdd: bddlib.BDD = field(default_factory=lambda: bddlib.BDD())
+
+    def __post_init__(self) -> None:
+        self.declare(self.variables)
+
+    def declare(self, variables: set[str]) -> None:
+        """Add variables to the polynomial context."""
+        new_vars = variables - self.variables
+        if len(new_vars) > 0:
+            self.bdd.declare(*new_vars)
+            self.variables.update(new_vars)
+
+
+@final
+class BooleanPolynomial(AbstractPolynomial[bool, Context]):
+    """A Polynomial over the Boolean algebra.
+
+    A Boolean polynomial is defined over the Boolean algebra, where addition is defined by logical OR and multiplication by
+    logical AND.
+    """
+
+    def __init__(
+        self,
+        manager: Optional[Context] = None,
+        *,
+        variables: Optional[Iterable[str]] = None,
+    ) -> None:
+        super().__init__()
+
+        variables = variables or {}
+        if manager:
+            self._manager = manager
+            self._manager.declare(set(variables))
+        else:
+            self._manager = Context(variables=set(variables))
+        self._expr: bddlib.Function = self._bdd.true
+
+    @property
+    def _bdd(self) -> bddlib.BDD:
+        return self._manager.bdd
+
+    @classmethod
+    def _wrap(cls, expr: bddlib.Function, manager: Context) -> "BooleanPolynomial":
+        poly = BooleanPolynomial(manager=manager)
+        poly._expr = expr
+        return poly
+
+    @property
+    @override
+    def context(self) -> Context:
+        return self._manager
+
+    @property
+    @override
+    def support(self) -> set[str]:
+        return self._bdd.support(self._expr)
+
+    @override
+    def declare(self, vars: set[str]) -> None:
+        self._manager.declare(vars)
+
+    @override
+    @classmethod
+    def zero(cls, manager: Optional[Context] = None) -> "BooleanPolynomial":
+        """Return a constant `0` polynomial"""
+        manager = manager or Context()
+        return cls._wrap(manager.bdd.false, manager)
+
+    @override
+    @classmethod
+    def const(cls, value: bool, manager: Optional[Context] = None) -> "BooleanPolynomial":
+        """Return a constant"""
+        poly = BooleanPolynomial(manager=manager)
+        if value is True:
+            poly._expr = poly._bdd.true
+        else:
+            poly._expr = poly._bdd.false
+        return poly
+
+    @override
+    def let(self, mapping: Mapping[str, bool | Self]) -> "BooleanPolynomial":
+        new_mapping = {name: val if isinstance(val, bool) else val._expr for name, val in mapping.items()}
+        new_func: bddlib.Function = self._bdd.let(new_mapping, self._expr)  # type: ignore
+        return self._wrap(new_func, self._manager)
+
+    @override
+    def eval(self, mapping: Mapping[str, bool]) -> bool:
+        assert self.support.issubset(mapping.keys())
+        evald = self.let(mapping)
+        if evald == self._bdd.true:
+            return True
+        elif evald == self._bdd.false:
+            return False
+        else:
+            raise RuntimeError("Evaluated polynomial is not constant, even with full support")
+
+    @override
+    def add(self, other: bool | Self) -> "BooleanPolynomial":
+        if isinstance(other, bool):
+            wrapped = self.const(other, manager=self._manager)
+        else:
+            wrapped = other
+        new_func = self._expr | wrapped._expr
+        return self._wrap(new_func, self._manager)
+
+    @override
+    def multiply(self, other: bool | Self) -> "BooleanPolynomial":
+        if isinstance(other, bool):
+            wrapped = self.const(other, manager=self._manager)
+        else:
+            wrapped = other
+        new_func = self._expr & wrapped._expr
+        return self._wrap(new_func, self._manager)
+
+    @override
+    def is_top(self) -> bool:
+        return self._expr == self._bdd.false
+
+    @override
+    def is_bottom(self) -> bool:
+        return self._expr == self._bdd.true
